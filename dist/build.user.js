1520488cdb2a420d9765ce23b0c87193e7e5a442
SkyCloud <88238944+SkyCloudDev@users.noreply.github.com>
2024-01-14 15:29:39 +0000

    Added bunkrr.ru

--- dist/build.user.js
+++ dist/build.user.js
@@ -6,7 +6,7 @@
 // @author x111000111
 // @author backwards
 // @description Downloads images and videos from posts
-// @version 2.8.0
+// @version 2.8.1
 // @updateURL https://github.com/SkyCloudDev/ForumPostDownloader/raw/main/dist/build.user.js
 // @downloadURL https://github.com/SkyCloudDev/ForumPostDownloader/raw/main/dist/build.user.js
 // @icon https://simp4.jpg.church/simpcityIcon192.png
@@ -27,6 +27,7 @@
 // @connect i-pizza.bunkr.ru
 // @connect bunkr.ru
 // @connect bunkr.su
+// @connect bunkrr.ru
 // @connect bunkrr.su
 // @connect bunkr.la
 // @connect bunkr.is
